65ee86e3a7fae1ab6a374628b92043034150d5a3
shamaryy113 <104206928+shamaryy113@users.noreply.github.com>
2022-04-22 15:34:45 +0300

    Add files via upload

---
+++ ar_config.lua
@@ -0,0 +1,113 @@
+>--------------------------------------------------------------------------<
+جميع الحقوق محفوظة لدى عرب تيم
+لتعديل اغلب الاشياء من الاسماء والروابط والاسم والى اخره يرجى التعديل على الملف الي تحت
+index.html
+لتعديل الالوان يرجى الذهاب الى 
+assets\css\style.css
+لتعديل الصور والافاتارات وصر المابات والسكربتات والشعار يرجى الذهاب الى
+assets\img
+>--------------------------------------------------------------------------<
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+>--------------------------------------------------------------------------<
+
+تمت برمجة هذا الموقع من قبل فولد
+Devloper by FOLD ©
+الموقع مجاني ولا يباع
+>--------------------------------------------------------------------------<
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
+
+ARAB TEAM
+https://discord.gg/A-T
+© Copyright ARAB TEAM. All Rights Reserved
